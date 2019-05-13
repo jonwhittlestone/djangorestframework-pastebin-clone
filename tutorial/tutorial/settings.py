0a23f085c775ca24804e5bc3fe3717184e28586d
Jon Whittlestone <jon@howapped.com>
2019-05-13 07:46:50 +0100

    Step 1b: Add `snippet_detail` route for GET, PUT and DELETE
    Using `httpie`: `http http://127.0.0.1:8000/snippets/7/`

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -72,6 +72,13 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
+
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 
